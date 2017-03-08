53f9a7e9c86f4cf8ac87d6c36090f7b0cb0f455b
cyan478 <cyan2@stuy.edu>
2017-03-08 12:34:20 -0500

    something about change color

--- svg.js
+++ svg.js
@@ -1,19 +1,34 @@
 var c = document.getElementById("vimage");
 var xmlns = "http://www.w3.org/2000/svg";
+var w = c.getBoundingClientRect().width
+var h = c.getBoundingClientRect().height
 
 
 function makeCircle(x,y){
     var c1 = document.createElementNS( xmlns, "circle");
     c1.setAttribute("cx",x)
     c1.setAttribute("cy",y)
-    c1.setAttribute("fill","red")
+    c1.setAttribute("fill","purple")
     c1.setAttribute("r","20")
     return c1;
 };
 
 var addCircle = function(event){
-    c.appendChild(makeCircle(event.offsetX,event.offsetY));
+    //if (event.target == this) ????
+        c.appendChild(makeCircle(event.offsetX,event.offsetY));
 };
+
+var changeColor = function(event){
+    this.setAttribute("fill","green")
+    this.addEventListener("click",remove);
+
+};
+
+var remove = function(event){
+    c.appendChild(makeCircle(int(Math.random() * w), int(Math.random() * h)));
+    c.removeChild(this);
+};
+
 
 var clearSVG = function(){
     console.log("hello");
@@ -21,6 +36,8 @@ var clearSVG = function(){
 		c.removeChild(c.lastChild);
 	};
 }
+
+
 
 c.addEventListener('click', addCircle);
 
