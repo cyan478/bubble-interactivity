5a01b0177091881d57c79d47aeb7b12382540ea0
Harry <lilharry221@gmail.com>
2017-03-09 21:45:16 -0500

    done

--- svg.js
+++ svg.js
@@ -2,14 +2,18 @@ var c = document.getElementById("vimage");
 var xmlns = "http://www.w3.org/2000/svg";
 var w = c.getBoundingClientRect().width
 var h = c.getBoundingClientRect().height
-
+var r = 20;
+var requestID;
+var animate = true;
 
 function makeCircle(x,y){
     var c1 = document.createElementNS( xmlns, "circle");
     c1.setAttribute("cx",x)
     c1.setAttribute("cy",y)
+    c1.setAttribute("dx",1)
+    c1.setAttribute("dy",1)
     c1.setAttribute("fill","purple")
-    c1.setAttribute("r","20")
+    c1.setAttribute("r",r)
     return c1;
 };
 
@@ -27,56 +31,58 @@ var changeColor = function(event){
 };
 
 var remove = function(event){
-    c.appendChild(makeCircle(Math.random() * w, Math.random() * h));
+    c.appendChild(makeCircle(Math.random() * (w-2*r) + r, Math.random() * (h-2*r) + r));
     c.removeChild(this);
     event.stopPropagation();
 };
 
-var move = function(){
 
-    var increasex = true;
-    var increasey = true;
-
-    var drawDot = function() {
-        //console.log(d.width.animVal)
-        //console.log(d.height.animVal)
+var moveC = function(){
+    if (animate){
+        animate=false;
         
-        d.setAttribute("x",xcor);
-        d.setAttribute("y",ycor);
-        if (xcor + 20 >= w){
-            increasex=false;
-        }
+        var moveDot = function() {
+            //console.log(d.width.animVal)
+            //console.log(d.height.animVal)
+            var circles = document.getElementsByTagName('circle')
+            var xcor,ycor,dx,dy;
+            
+            for (var i = 0; i < circles.length;i++){
+                circle = circles[i]
+                xcor = parseInt(circle.getAttribute("cx"));
+                ycor = parseInt(circle.getAttribute("cy"));
+                dx = parseInt(circle.getAttribute("dx"));
+                dy = parseInt(circle.getAttribute("dy"));
+                
+                if (xcor + r >= w){
+                    dx=-1
+                }
 
-        if (xcor - 20<= 0){
-            increasex=true;
-        }
+                if (xcor - r<= 0){
+                    dx=1;
+                }
 
-        if (ycor + 20 >= h){
-            increasey=false;
-        }
+                if (ycor + r >= h){
+                    dy=-1;
+                }
 
-        if (ycor - 20 <= 0){
-            increasey=true;
-        }
+                if (ycor - r <= 0){
+                    dy=1;
+                }
+                circle.setAttribute("cx",xcor+dx) 
+                circle.setAttribute("cy",ycor+dy)
+                circle.setAttribute("dx",dx)
+                circle.setAttribute("dy",dy)
+            };
+            requestID = window.requestAnimationFrame( moveDot );
 
-        if (increasex){
-            xcor++;
-        }else{
-            xcor--;
-        }
-
-        if (increasey){
-            ycor++;
-        }else{
-            ycor--;
-        }  
-
-        requestID = window.requestAnimationFrame( drawDot );
-
+        };
+        moveDot();
+    }else{
+        window.cancelAnimationFrame(requestID);
+        animate=true
     }
-    drawDot();
 };
-
 
 var clearSVG = function(){
     console.log("hello");
@@ -91,3 +97,7 @@ c.addEventListener('click', addCircle);
 
 var clear = document.getElementById( "clear-but" );
 clear.addEventListener("click", clearSVG);
+
+var move = document.getElementById( "move-but" );
+move.addEventListener("click", moveC);
+
