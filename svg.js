2aa5adc27e796daf3e569160a3f2555d7a7f8e1d
lilharry <hlum1@stuy.edu>
2017-03-20 12:21:19 -0400

    done

--- svg.js
+++ svg.js
@@ -1,124 +1,112 @@
-var c = document.getElementById("vimage");
-var xmlns = "http://www.w3.org/2000/svg";
-var w = c.getBoundingClientRect().width
-var h = c.getBoundingClientRect().height
-var requestID;
-var animate = true;
+var addCircle, animate, change, height, makeCircle, move, rid, svg, width, animating;
 
-function makeCircle(x,y,r){
-    var c1 = document.createElementNS( xmlns, "circle");
-    c1.setAttribute("cx",x)
-    c1.setAttribute("cy",y)
-    c1.setAttribute("dx",1)
-    c1.setAttribute("dy",1)
-    c1.setAttribute("fill","purple")
-    c1.setAttribute("r",r)
-    return c1;
+svg = document.getElementById('svg');
+
+move = document.getElementById('move');
+
+height = svg.getAttribute('height');
+
+width = svg.getAttribute('width');
+
+animating = false;
+
+rid = void 0;
+
+change = function(e) {
+  if (this.getAttribute('fill') === 'green') {
+    svg.appendChild(makeCircle(Math.random() * width, Math.random() * height, 40));
+    svg.removeChild(this);
+    return;
+  }
+  this.setAttribute('fill', 'green');
+  return e.stopPropagation();
 };
 
-var addCircle = function(x,y,r){
-    //if (event.target == this) ????
-    circle = makeCircle(x,y,r)
-    c.appendChild(circle);
-    circle.addEventListener('click', changeColor);
-    return circle
+makeCircle = function(x, y, r) {
+  var c;
+  c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  c.setAttribute('cx', x);
+  c.setAttribute('cy', y);
+  c.setAttribute('r', r);
+  c.setAttribute('fill', 'red');
+  c.setAttribute('stroke', 'black');
+  c.setAttribute('dx', 1);
+  c.setAttribute('dy', 1);
+  c.addEventListener('click', change);
+  return c;
 };
 
-var changeColor = function(event){
-    this.setAttribute("fill","green")
-    this.addEventListener("click",remove);
-    event.stopPropagation();
+addCircle = function(e) {
+  if (this === e.target) {
+    return svg.appendChild(makeCircle(e.offsetX, e.offsetY, 40));
+  }
 };
 
-var remove = function(event){
-    c.appendChild(makeCircle(Math.random() * (w-2*r) + r, Math.random() * (h-2*r) + r));
-    c.removeChild(this);
-    event.stopPropagation();
-};
-
-
-var moveC = function(){
-    if (animate){
-        animate=false;
-        
-        var moveDot = function() {
-            //console.log(d.width.animVal)
-            //console.log(d.height.animVal)
-            var circles = document.getElementsByTagName('circle')
-	    circles = [].slice.call(circles)
-            var xcor,ycor,dx,dy;
-	    var done;
-            
-            for (var i = 0; i < circles.length;i++){
-		done = false;
-                circle = circles[i]
-                xcor = parseInt(circle.getAttribute("cx"));
-                ycor = parseInt(circle.getAttribute("cy"));
-                dx = parseInt(circle.getAttribute("dx"));
-                dy = parseInt(circle.getAttribute("dy"));
-		r = parseInt(circle.getAttribute("r"));
-                
-                if (xcor + r >= w){
-                    dx=-1
-                }
-
-                if (xcor - r<= 0){
-                    dx=1;
-                }
-
-                if (ycor + r >= h){
-                    dy=-1;
-                }
-
-                if (ycor - r <= 0){
-                    dy=1;
-                }
-
-		if (xcor == w/2 && done==false){
-		    circle.setAttribute("r",r/2);
-		    if (r <= 2){
-			c.removeChild(circle);
-			i-=1;
-		    }else{
-			newCircle = addCircle(xcor,ycor);
-			newCircle.setAttribute("dx",-1);
-			newCircle.setAttribute("dy",-1);
-			newCircle.setAttribute("r",r/2);
-			c.appendChild(newCircle);
-		    }
-		    done = true;
+animate = function(e) {
+    if (!animating){
+	animating=true;
+	var moveCircle;
+	window.cancelAnimationFrame(rid);
+	moveCircle = function() {
+	    var circ, circles, curx, cury, dx, dy, i, newDot, r;
+	    circles = document.getElementsByTagName('circle');
+	    i = 0;
+	    while (i < circles.length) {
+		circ = circles[i];
+		curx = parseInt(circ.getAttribute('cx'));
+		cury = parseInt(circ.getAttribute('cy'));
+		dx = parseInt(circ.getAttribute('dx'));
+		dy = parseInt(circ.getAttribute('dy'));
+		r = parseInt(circ.getAttribute('r'));
+		if (curx === width / 2) {
+		    circ.setAttribute('r', r / 2);
+		    newDot = makeCircle(curx + dx * -1, cury, r / 2);
+		    newDot.setAttribute('dx', dx * -1);
+		    newDot.setAttribute('dy', dy * -1);
+		    svg.appendChild(newDot);
 		}
-                circle.setAttribute("cx",xcor+dx) 
-                circle.setAttribute("cy",ycor+dy)
-                circle.setAttribute("dx",dx)
-                circle.setAttribute("dy",dy)
-            };
-            requestID = window.requestAnimationFrame( moveDot );
-
-        };
-        moveDot();
+		if (r <= 2) {
+		    svg.removeChild(circ);
+		}
+		if (curx >= width - r || curx <= r) {
+		    circ.setAttribute('dx', dx * -1);
+		}
+		if (cury >= height - r || cury <= r) {
+		    circ.setAttribute('dy', dy * -1);
+		}
+		curx += parseInt(circ.getAttribute('dx'));
+		cury += parseInt(circ.getAttribute('dy'));
+		circ.setAttribute('cx', curx);
+		circ.setAttribute('cy', cury);
+		i++;
+	    }
+	    rid = window.requestAnimationFrame(moveCircle);
+	};
+	return moveCircle();
     }else{
-        window.cancelAnimationFrame(requestID);
-        animate=true
+	window.cancelAnimationFrame(rid);
+	animating=false;
     }
 };
 
+
+
+
+
 var clearSVG = function(){
     console.log("hello");
-	while (c.lastChild) {
-		c.removeChild(c.lastChild);
-	};
+    while (svg.lastChild) {
+	svg.removeChild(svg.lastChild);
+    };
 }
 
 
 
-c.addEventListener('click', function(e){
-    addCircle(e.offsetX,e.offsetY,40)
-});
+svg.addEventListener('click', addCircle);
 
-var clear = document.getElementById( "clear-but" );
+clear = document.getElementById( "clear-but" );
 clear.addEventListener("click", clearSVG);
 
-var move = document.getElementById( "move-but" );
-move.addEventListener("click", moveC);
+move = document.getElementById( "move-but" );
+move.addEventListener("click", animate);
 
